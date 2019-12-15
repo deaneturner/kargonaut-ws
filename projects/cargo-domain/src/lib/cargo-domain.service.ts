cb1f2a22ac47b97d0e426d262bf6eef64d5e790c
deaneturner <service@deaneturner.com>
2019-12-14 19:42:35 -0500

    add conceptual methods

--- projects/cargo-domain/src/lib/cargo-domain.service.ts
+++ projects/cargo-domain/src/lib/cargo-domain.service.ts
@@ -6,4 +6,27 @@ import { Injectable } from '@angular/core';
 export class CargoDomainService {
 
   constructor() { }
+
+  /**
+   * Unique Tag Generator
+   * Generate a unique ID (tag).
+   */
+  generateUniqueTagId(): string {
+    return '1';
+  }
+
+  /**
+   * Package-Item Generator
+   * Generate a new package item, the single entity that manages no others.
+   */
+
+  /**
+   * Package Generator
+   * Generate a new package that can manage multiple package items.
+   */
+
+  /**
+   * Cargo Generator
+   * Generate a new cargo hold that can manage multiple packages.
+   */
 }
