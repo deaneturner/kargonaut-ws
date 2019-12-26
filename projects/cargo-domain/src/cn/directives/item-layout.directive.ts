10d2403ec04b355add6691252639b4b3e914e7ba
deaneturner <service@deaneturner.com>
2019-12-26 13:21:02 -0500

    first pass styling

--- projects/cargo-domain/src/cn/directives/item-layout.directive.ts
+++ projects/cargo-domain/src/cn/directives/item-layout.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[cnItemLayout]'
 })
 export class ItemLayoutDirective {
 
-  constructor() { }
+  @HostBinding('class.cn-item')
+  get cssClasses() {
+    return true;
+  }
+
+  constructor() {
+    console.log('Item Layout Directive');
+  }
 
 }
