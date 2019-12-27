c1717f417850dc2f0dc14cbf8ea30c0941a6d359
deaneturner <service@deaneturner.com>
2019-12-27 16:21:12 -0500

    template config for packages

--- projects/cargo-domain/src/cn/cargo.module.ts
+++ projects/cargo-domain/src/cn/cargo.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CargoComponent } from './components/cargo.component';
 import { InlineGridDirective } from './directives/inline-grid.directive';
 import { PackageComponent } from './components/package/package.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [CargoComponent, InlineGridDirective, PackageComponent],
   imports: [
+      CommonModule
   ],
   exports: [CargoComponent, InlineGridDirective, PackageComponent]
 })
