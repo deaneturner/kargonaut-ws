842fe6e0065d9f520bf1ec70f927a42fa9b116a4
deaneturner <service@deaneturner.com>
2019-12-27 14:30:12 -0500

    refined configuration and render

--- projects/demo/src/app/package-example/package-example.component.ts
+++ projects/demo/src/app/package-example/package-example.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PackageConfig } from '../../../../cargo-domain/src/models/Package.config';
 import { ItemConfig } from '../../../../cargo-domain/src/models/Item.config';
 import { items } from '../../assets/data/items';
+import { Item } from '../../../../cargo-domain/src/models/Item';
 
 @Component({
   selector: 'demo-package-example',
@@ -13,9 +14,12 @@ export class PackageExampleComponent implements OnInit {
   packageConfig: PackageConfig;
   itemConfig: ItemConfig;
 
+  items: Item[];
+
   constructor() { }
 
   ngOnInit() {
+    this.items = items;
     this.packageConfig = {
       cnData: {
         cnLabel: 'Package Component',
