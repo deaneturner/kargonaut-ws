404c54c5fba8e03aea966b3786233d932132505e
deaneturner <service@deaneturner.com>
2020-01-02 02:14:22 -0500

    enable separate heroku instance for documentation

--- server-docs.js
+++ server-docs.js
@@ -5,16 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/demo"));
-
-app.get("/docs/*", function(req, res) {
-
-  res.sendFile(path.join(__dirname + "/documentation/index.html"));
-});
+app.use(express.static(__dirname + "/documentation"));
 
 app.get("/*", function(req, res) {
 
-  res.sendFile(path.join(__dirname + "/dist/demo/index.html"));
+  res.sendFile(path.join(__dirname + "/documentation/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
