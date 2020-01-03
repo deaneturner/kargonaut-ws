4d179ef7e0253d8d16d743290e9d915b254504e4
deaneturner <service@deaneturner.com>
2020-01-02 20:34:28 -0500

    fail attempt at async pipe test

--- projects/demo/src/app/components/component-example/component-example.component.spec.ts
+++ projects/demo/src/app/components/component-example/component-example.component.spec.ts
@@ -4,6 +4,8 @@ import { ComponentExampleComponent } from './component-example.component';
 import { NgGistModule } from 'ng-gist';
 import { PackageComponent } from '../../../../../cargo-domain/src/cn/components/package/package.component';
 import { InlineGridDirective } from '../../../../../cargo-domain/src/cn/directives/inline-grid/inline-grid.directive';
+import { items } from '../../../assets/data/items';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('ComponentExampleComponent', () => {
   let component: ComponentExampleComponent;
@@ -26,10 +28,11 @@ describe('ComponentExampleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ComponentExampleComponent);
     component = fixture.componentInstance;
+    component.items$ = of(items);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create', async(() => {
+      expect(component).toBeTruthy();
+  }));
 });
