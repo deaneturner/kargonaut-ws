30dda313fb24499eba5cd2229982ecba1b7c511b
deaneturner <service@deaneturner.com>
2020-01-07 02:21:31 -0500

    about in nav

--- projects/demo/src/app/examples.ts
+++ projects/demo/src/app/examples.ts
@@ -3,8 +3,10 @@ import { BasicStylingExampleComponent } from './components/basic-styling-example
 import { DirectivesExampleComponent } from './components/directives-example/directives-example.component';
 import { TemplateExampleComponent } from './components/template-example/template-example.component';
 import { ComponentExampleComponent } from './components/component-example/component-example.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const examples = [
+  { path: 'About', component: AboutComponent },
   { path: 'markup-example', component: SimpleMarkupExampleComponent },
   { path: 'basic-styling-example', component: BasicStylingExampleComponent },
   { path: 'directives-example', component: DirectivesExampleComponent },
