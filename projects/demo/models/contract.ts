497504e3feb0f74da6e8626ba4a1ca22848de146
deaneturner <service@deaneturner.com>
2020-01-08 15:10:36 -0500

    add an addition field to contract for item interface violation test

--- projects/demo/models/contract.ts
+++ projects/demo/models/contract.ts
@@ -1,6 +1,7 @@
 import { Item } from '../../cargo-domain/src/models/Item';
 
 export interface Contract extends Item {
-    isSelected: boolean;
-    checkbox: string;
+    isSelected?: boolean;
+    checkbox?: string;
+    duration?: string;
 }
--- projects/demo/src/assets/data/contracts.ts
+++ projects/demo/src/assets/data/contracts.ts
@@ -7,20 +7,7 @@ export const contracts: Contract[] = [
         knDescription: '',
         isSelected: false,
         checkbox: 'check_box_outline_blank',
-    },
-    {
-        knTag: 'f0529af2-e961-4565-8515-bd453efca424',
-        knLabel: 'Contract B',
-        knDescription: '',
-        isSelected: false,
-        checkbox: 'check_box_outline_blank',
-    },
-    {
-        knTag: '17e4371e-be8b-473e-9da3-7d6c757b7a3b',
-        knLabel: 'Contract C',
-        knDescription: '',
-        isSelected: false,
-        checkbox: 'check_box_outline_blank',
+        duration: '1 day',
     },
     {
         knTag: '844a9381-7a51-40e7-affb-fdf74eb280b3',
@@ -28,6 +15,23 @@ export const contracts: Contract[] = [
         knDescription: '',
         isSelected: false,
         checkbox: 'check_box_outline_blank',
+        duration: '7 days',
+    },
+    {
+        knTag: 'f0529af2-e961-4565-8515-bd453efca424',
+        knLabel: 'Contract B',
+        knDescription: '',
+        isSelected: false,
+        checkbox: 'check_box_outline_blank',
+        duration: '1 week',
+    },
+    {
+        knTag: '17e4371e-be8b-473e-9da3-7d6c757b7a3b',
+        knLabel: 'Contract C',
+        knDescription: '',
+        isSelected: false,
+        checkbox: 'check_box_outline_blank',
+        duration: '1 year',
     }
 ];
 
