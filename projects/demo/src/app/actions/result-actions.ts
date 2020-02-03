976033fadc54c942743fb756878919f2d84b12bc
deaneturner <service@deaneturner.com>
2020-02-03 16:52:47 -0500

    fix action detail descr.

--- projects/demo/src/app/actions/result-actions.ts
+++ projects/demo/src/app/actions/result-actions.ts
@@ -2,8 +2,8 @@ import { Tag } from '../../../../cargo-domain/src/common/TagGenerator';
 import { createAction, props } from '@ngrx/store';
 import { Result } from '../../../models/result';
 
-export const appendResult = createAction('[Result[ Append Result', props<{ item: Result }>());
-export const replaceResult = createAction('[Result[ Replace Result', props<{ item: Result }>());
-export const deleteResult = createAction('[Result[ Delete Result', props<{ knTag: Tag }>());
-export const editResult = createAction('[Result[ Edit Result', props<{ knTag: Tag }>());
-export const cancelResult = createAction('[Result[ Cancel Result');
+export const appendResult = createAction('[Result] Append Result', props<{ item: Result }>());
+export const replaceResult = createAction('[Result] Replace Result', props<{ item: Result }>());
+export const deleteResult = createAction('[Result] Delete Result', props<{ knTag: Tag }>());
+export const editResult = createAction('[Result] Edit Result', props<{ knTag: Tag }>());
+export const cancelResult = createAction('[Result] Cancel Result');
