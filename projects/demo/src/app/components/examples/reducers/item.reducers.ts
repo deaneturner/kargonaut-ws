f0fe0b47c77d016316dbc7576b2a9f94a8319808
deaneturner <service@deaneturner.com>
2020-02-18 17:35:17 -0500

    instantiate redux resources

---
+++ projects/demo/src/app/components/examples/reducers/item.reducers.ts
@@ -0,0 +1,26 @@
+import { appendItem, deleteItem, replaceItem } from '../actions/item.actions';
+import { createReducer } from '@ngrx/store';
+import { on } from '@ngrx/store';
+import { ItemExample } from '../../../../../models/item-example';
+import { Item } from '../../../../../../cargo-domain/src/kn/models/Item';
+import { TagGenerator } from '../../../../../../cargo-domain/src/common/TagGenerator';
+import { items } from '../../../../assets/data/items';
+
+const initialItems: Item[] = items;
+
+export function itemsReducer(state: any | undefined, action: any) {
+    return createReducer<ItemExample[]>(
+        initialItems,
+        on(appendItem, (s, a) => s.concat({
+            ...a.item,
+            knTag: TagGenerator.generateId(),
+        })),
+        on(replaceItem, (s, a) => {
+            const newItems = s.concat();
+            newItems[newItems.findIndex(item => item.knTag === a.item.knTag)] = a.item;
+            return newItems;
+        }),
+        on(deleteItem, (s, a) => s.filter(
+            item => item.knTag !== a.knTag)),
+    )(state, action);
+}
---
+++ projects/demo/src/app/components/examples/reducers/results.reducers.ts
@@ -0,0 +1,34 @@
+import { results } from '../../../../assets/data/results';
+import { Tag, TagGenerator } from '../../../../../../cargo-domain/src/common/TagGenerator';
+import { appendResult, cancelResult, deleteResult, editResult, replaceResult } from '../actions/result.actions';
+import { Action, createReducer } from '@ngrx/store';
+import { on } from '@ngrx/store';
+import { Result } from '../../../../../models/result';
+
+const initialResults: Result[] = results;
+
+export function resultsReducer(state: Result[] | undefined, action: Action) {
+    return createReducer<Result[]>(
+        initialResults,
+        on(appendResult, (s, a) => s.concat({
+            ...a.result,
+            knTag: TagGenerator.generateId(),
+        })),
+        on(replaceResult, (s, a) => {
+            const newResults = s.concat();
+            newResults[newResults.findIndex(result => result.knTag === a.result.knTag)] = a.result;
+            return newResults;
+        }),
+        on(deleteResult, (s, a) => s.filter(
+            result => result.knTag !== a.knTag)),
+    )(state, action);
+}
+
+export function editResultTagReducer(state: string | undefined, action: Action) {
+    return createReducer<Tag>(undefined,
+        on(editResult, (_, a) => a.knTag),
+        on(replaceResult, () => undefined),
+        on(deleteResult, () => undefined),
+        on(cancelResult, () => undefined),
+    )(state, action);
+}
