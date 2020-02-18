982c80d719217fef74f72ccc6f5ff37bfaef981c
deaneturner <service@deaneturner.com>
2020-02-18 18:15:50 -0500

    reinstate about page

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'examples',
     loadChildren: () => import('./components/examples/examples.module').then(m => m.ExamplesModule)
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   },
   {
     path: '**',
