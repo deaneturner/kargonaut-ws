f8fe77af1333086b93ac5ed12c59c9bd9719e1c4
deaneturner <service@deaneturner.com>
2020-02-18 18:10:57 -0500

    all working with new stores

--- projects/demo/src/app/components/examples/template-example/template-example.component.ts
+++ projects/demo/src/app/components/examples/template-example/template-example.component.ts
@@ -7,65 +7,67 @@ import { select, Store } from '@ngrx/store';
 import { AppState } from '../../../app.state';
 import { ItemExample } from '../../../../../models/item-example';
 import { Contract } from '../../../../../models/contract';
+import { replaceItem } from '../actions/item.actions';
 
 @Component({
-  selector: 'demo-template-example',
-  templateUrl: './template-example.component.html',
-  styleUrls: ['./template-example.component.scss']
+    selector: 'demo-template-example',
+    templateUrl: './template-example.component.html',
+    styleUrls: ['./template-example.component.scss']
 })
 export class TemplateExampleComponent implements OnInit {
 
-  static label = 'Templates';
+    static label = 'Templates';
 
-  @Output()
-  selected: EventEmitter<Contract> = new EventEmitter();
+    @Output()
+    selected: EventEmitter<Contract> = new EventEmitter();
 
-  packageConfig: PackageConfig;
-  packageConfigSingle: PackageConfig;
-  itemConfig: ItemConfig;
+    packageConfig: PackageConfig;
+    packageConfigSingle: PackageConfig;
+    itemConfig: ItemConfig;
 
-  items$: Observable<Item[]> = this.store.pipe(select(state => state.example_items));
+    items$: Observable<Item[]> = this.store.pipe(select(state => state.example_items));
 
-  layout = {
-    selected: {
-      true: 'check_box',
-      false: 'check_box_outline_blank'
+    layout = {
+        selected: {
+            true: 'check_box',
+            false: 'check_box_outline_blank'
+        }
+    };
+
+    constructor(private store: Store<AppState>) {
     }
-  };
 
-  constructor(private store: Store<AppState>) { }
+    ngOnInit() {
+        this.packageConfig = {
+            knLabel: 'Package Component',
+            knCount: 4,
+            knLayout: {
+                knColumns: ['30px', 'auto', '60px'],
+                knMaxCollapse: 4
+            }
+        };
+        this.packageConfigSingle = {
+            knLabel: 'Package Component',
+            knCount: 1,
+            knLayout: {
+                knColumns: ['30px', 'auto', '60px'],
+                knMaxCollapse: 4
+            }
+        };
+        this.itemConfig = {
+            knData: this.items$,
+            knLayout: {
+                knColumns: ['30px', '30px', 'auto', '60px', '30px', '30px', '60px']
+            }
+        };
+    }
 
-  ngOnInit() {
-    this.packageConfig = {
-      knLabel: 'Package Component',
-      knCount: 4,
-      knLayout: {
-        knColumns: ['30px', 'auto', '60px'],
-        knMaxCollapse: 4
-      }
-    };
-    this.packageConfigSingle = {
-      knLabel: 'Package Component',
-      knCount: 1,
-      knLayout: {
-        knColumns: ['30px', 'auto', '60px'],
-        knMaxCollapse: 4
-      }
-    };
-    this.itemConfig = {
-      knData: this.items$,
-      knLayout: {
-        knColumns: ['30px', '30px', 'auto', '60px', '30px', '30px', '60px']
-      }
-    };
-  }
+    get label() {
+        return TemplateExampleComponent.label;
+    }
 
-  get label() {
-    return TemplateExampleComponent.label;
-  }
-
-  onSelect(item: ItemExample) {
-    item.isSelected = !item.isSelected;
-    this.selected.emit(item);
-  }
+    onSelect(item: ItemExample) {
+        this.store.dispatch(replaceItem({item: {...item, isSelected: !item.isSelected}}));
+        this.selected.emit(item);
+    }
 }
